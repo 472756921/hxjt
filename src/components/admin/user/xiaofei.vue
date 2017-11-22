b57065472aefbdae88b50c89270c5d63852a0eda
Benson <472756921@qq.com>
2017-11-22 15:37:23 +0800

    change

--- src/components/admin/user/xiaofei.vue
+++ src/components/admin/user/xiaofei.vue
@@ -4,10 +4,14 @@
     <el-radio v-model="radio" label="3">商品消费记录</el-radio>
     <el-radio v-model="radio" label="4">在线问诊记录</el-radio>
     <el-radio v-model="radio" label="5">预约面对面记录</el-radio>
+    <!--<el-radio v-model="radio" label="6">剩余服务项目</el-radio>-->
     <fubList v-if="radio==2" :userID='userID'/>
     <spList v-if="radio==3" :userID='userID'/>
     <wzList v-if="radio==4" :userID='userID'/>
     <yu v-if="radio==5" :userID='userID'/>
+    <!--<div v-for="(it, i) in serviceTime" v-if="radio==6">-->
+      <!--{{it.service_name}} - {{it.service_count}} 次-->
+    <!--</div>-->
   </div>
 </template>
 
@@ -16,6 +20,7 @@
   import spList from './userdata/spList.vue';
   import wzList from './userdata/wzList.vue';
   import yu from './userdata/yu.vue';
+  import {getGroupCustomerMessage} from '../../interface';
 
   export default {
     name: 'xiaofei',
@@ -33,9 +38,18 @@
         tableData: [],
         pageNow: 1,
         pageTotle: 1,
+        serviceTime: [],
       }
     },
     methods:{
+      get(){
+        this.$ajax({
+          method: 'GET',
+          url: getGroupCustomerMessage()+"?customer_id="+this.userID,
+        }).then((res) => {
+          this.serviceTime = res.data.service_detail;
+        })
+      }
     },
   };
 </script>
